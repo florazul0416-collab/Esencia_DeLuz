44248f61bce4bb77d8ece0e4b55f6eb5d35c8eb8
Flor Andrea Vasquez Sanchez <florazul0416@gmail.com>
2026-03-18 10:46:56 -0500

    Add files via upload

--- assets/js/api-client.js
+++ assets/js/api-client.js
@@ -1,3 +1,4 @@
+```javascript
 // Detección dinámica de la URL del servidor
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
     ? 'http://localhost:5000/api'
